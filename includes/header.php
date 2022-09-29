d1581cf06e85006639b32651ab4cc915ddc9c8c2
alexis-newton <114688521+alexis-newton@users.noreply.github.com>
2022-09-29 13:21:12 -0400

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<?php
+  include("includes/config.php");
+  include("includes/classes/User.php");
+  include("includes/classes/Artist.php");
+  include("includes/classes/Album.php");
+  include("includes/classes/Song.php");
+  include("includes/classes/Playlist.php");
+
+  if (isset($_SESSION['userLoggedIn'])) {
+    $userLoggedIn = new User($con, $_SESSION['userLoggedIn']);
+    $username = $userLoggedIn->getUsername();
+    echo "<script>userLoggedIn = '$username';</script>";
+  }
+  else {
+    header("Location: register.php");
+  }
+?>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome to Spotify!</title>
+
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="assets/js/script.js"></script>
+  </head>
+
+  <body>
+
+
+
+    <div id="mainContainer">
+
+      <div id="topContainer">
+        <?php include("includes/navBarContainer.php"); ?>
+
+        <div id="mainViewContainer">
+          <div id="mainContent">
+
+
+
+          </div>
